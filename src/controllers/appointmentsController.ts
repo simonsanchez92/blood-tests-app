9f4b7b06a863f1eed3c0888314bf20bef8da183c
Simon Sanchez <simonsanchez.1992@gmail.com>
2022-02-02 16:38:07 -0300

    working on appointments endpoints

--- src/controllers/appointmentsController.ts
+++ src/controllers/appointmentsController.ts
@@ -3,10 +3,22 @@ import { Appointment, IAppointment } from "../models/Appointment";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { generator } from "../utils/timeGenerator";
+import { User } from "../models/User";
 
 class AppointmentsController {
   public async generateOne(req: Request, res: Response, next: NextFunction) {
     const { day } = req.body;
+
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+
+    if (new Date(day) < now) {
+      return res.status(200).json({
+        success: false,
+        msg: "Date cannot be in the past",
+      });
+    }
+
     const timeSlots = generator(10);
     let dayAppointments: IAppointment[] = [];
 
@@ -22,12 +34,13 @@ class AppointmentsController {
         })
       );
     }
+
     try {
-      Appointment.insertMany(dayAppointments, {}, function (error, docs) {
-        return res.status(200).json({
-          msg: "Success",
-          appointment: docs,
-        });
+      const insertedDocs = await Appointment.insertMany(dayAppointments);
+
+      return res.status(200).json({
+        msg: "Success",
+        appointment: insertedDocs,
       });
     } catch (e) {
       console.error(e as Error);
@@ -44,6 +57,53 @@ class AppointmentsController {
       res.json({
         msg: "Success",
         appointments,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  public async makeAppointment(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const userId = req.body.userId;
+    const appointmentId = req.params.appointmentId;
+
+    const user = await User.findById(userId);
+    const appointment = await Appointment.findById(appointmentId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "User does not exist",
+      });
+    }
+
+    if (!appointment) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "Appointment does not exist" });
+    }
+
+    if (appointment?.userId !== null) {
+      return res.status(400).json({
+        success: false,
+        msg: "Appointment already taken",
+      });
+    }
+
+    try {
+      const updatedAppointment = await Appointment.findOneAndUpdate(
+        { _id: appointment._id },
+        { userId: user._id },
+        { new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        updatedAppointment,
       });
     } catch (e) {
       console.error(e);
--- src/routes/appointments.ts
+++ src/routes/appointments.ts
@@ -4,5 +4,6 @@ import { appointmentsController } from "../controllers/appointmentsController";
 
 router.post("/", appointmentsController.generateOne);
 router.get("/", appointmentsController.getAppointments);
+router.patch("/:appointmentId", appointmentsController.makeAppointment);
 
 export default router;
